7e80513beae7f9e5b81cc1c4cd9c939ea02ab1ab
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2020-04-27 11:58:21 -0300

    Arrumado problema na listagem de sugestões

--- AmigoOculto.java
+++ AmigoOculto.java
@@ -42,9 +42,9 @@ public class AmigoOculto {
 							}
 							//grupos
 							case 2:{
-								int opcaoEscolhidaGrupos;
+								int opcaoEscolhidaGrupos = 0;
 								do{
-									opcaoEscolhidaGrupos=menuLogadoGrupos(gruposAmigoOculto);
+									//opcaoEscolhidaGrupos = menuLogadoGrupos(gruposAmigoOculto);
 								}while(opcaoEscolhidaGrupos!=0);
 								break;
 							}
@@ -89,6 +89,7 @@ public class AmigoOculto {
 		int opcaoEscolhida=controladorPrograma.escolherOpcaoMenuInicial();
 		return opcaoEscolhida;
 	}
+
 	public static int menuLogadoGrupos(CRUD<Grupos> amigoOculto) {
 		int opcao = 0;
 		try {
@@ -119,6 +120,7 @@ public class AmigoOculto {
 		}
 		return opcao;
 	}
+
 	public static int menuLogado(CRUD<Sugestao> amigoOculto) {
 		int opcaoEscolhidaSugestoes = 0;
 		try {
@@ -215,7 +217,7 @@ public class AmigoOculto {
 			System.out.println();
 
 			//loop que vai procurar os n elementos retornados dentro do banco e printar os valores
-			for (int i = 0; i < idsSugestoes.length; i++, contador++) {
+			for (int i = 1; i <= idsSugestoes.length; i++, contador++) {
 				Sugestao temp = amigoOculto.read(i);
 
 				if (temp != null) {
